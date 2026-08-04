b2f939fdb0813f0ef378b2d4995f70cde6d558c3
negicodes <negiyogendra4339@gmail.com>
2026-08-05 03:14:41 +0530

    day 1

---
+++ sql day1.sql
@@ -0,0 +1,31 @@
+/* What is sql?
+
+SQL (Structured Query Language) is the standard programming language used to communicate , manage, and manipulate relational databases.
+Instead of opening a massive spreadsheet and manually searching through rows, you write plain, English-like SQL commands
+telling the database what data to retrieve, insert, update, or delete.
+
+*/
+
+Key Reasons We Need SQL
+
+Handles Massive Scale :
+Unlike spreadsheets (Excel) that freeze with large datasets, SQL efficiently manages millions to billions of rows of data.
+
+Lightning-Fast Querying : 
+Finds, filters, and retrieves specific data points in milliseconds using optimized indexing.
+
+Powers App Backends (CRUD): 
+Drives all real-world application functions:
+
+Create: Adding new records (e.g., signing up for an account)
+
+Read: Fetching data (e.g., viewing a profile or feed)
+
+Update: Modifying existing data (e.g., changing a password)
+
+Delete: Removing data (e.g., deleting a post)
+
+Connects Complex Data: 
+Easily joins linked tables together (e.g., combining Users, Orders, and Products).
+
+
